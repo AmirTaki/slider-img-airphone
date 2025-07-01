8ad7c7c049dc4e135904dfced583afe9f1d7de8e
amir <amirtaki623@gmail.com>
2025-07-02 02:05:52 +0330

    show slider js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,22 @@ let carousel = document.querySelector(".carousel")
 let listHTML = document.querySelector('.carousel .list')
 let seeMoreButtons = document.querySelectorAll('seeMore')
 let backButton = document.getElementById("back")
+
+nextButton.addEventListener('click' , (e)=> {
+    showSlider('next')
+})
+prevButton.addEventListener('click' , (e)=> {
+    showSlider('back')
+})
+
+const showSlider = (type) => {
+    let items = document.querySelectorAll(".item")
+    if(type == "next"){
+        listHTML.appendChild(items[0])
+        carousel.classList.add('next')
+    }
+    else {
+        listHTML.prepend(items[items.length - 1])
+        carousel.classList.add('prev')
+    }
+}
