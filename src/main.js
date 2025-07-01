1810b055f06ef65251afb06aff8bce7f9231b56d
amir <amirtaki623@gmail.com>
2025-07-02 02:02:44 +0330

    backbutton

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let nextButton = document.getElementById("next")
+let prevButton = document.getElementById('prev')
+let carousel = document.querySelector(".carousel")
+let listHTML = document.querySelector('.carousel .list')
+let seeMoreButtons = document.querySelectorAll('seeMore')
+let backButton = document.getElementById("back")
